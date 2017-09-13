2504f874c8c4888aa63036bd5621cd9fa870b154
sonoilconte <shammersinger@yahoo.com>
2017-09-13 13:37:03 -0700

    post new user working

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,17 +1,24 @@
 let db = require("../models")
 
 function index(req, res){
-  // db.User.find({}, function(err, users){
-  //   if (err){
-  //     console.log("ERROR", err);
-  //   }
-  //   console.log("GETTING ALL USERS")
-  //   res.json(users);
-  // });
-  res.json("hello from usersController index")
+  db.User.find({}, function(err, users){
+    if (err){
+      console.log("ERROR FINDING USERS", err);
+    }
+    console.log("GETTING ALL USERS")
+    res.json(users);
+  });
 }
 
 function create(req, res){
+  db.User.create(req.body, function(err, user){
+    if (err){
+      console.log("ERROR CREATING USER", err);
+    }
+    console.log("req.body is ", req.body)
+    console.log("CREATED USER", user);
+    res.json(user);
+  });
 }
 
 function show(req, res){
--- server.js
+++ server.js
@@ -8,9 +8,15 @@ let app = express();
 // access controllers
 let controllers = require("./controllers");
 
+// use body parser
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
 app.get("/api", controllers.api.index);
 
 app.get("/users", controllers.users.index);
+app.post("/users", controllers.users.create);
+
 app.get("/user", controllers.users.show)
 
 app.get("/texts", controllers.texts.index);
