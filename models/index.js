c8f4fd731c1073c072c10d0cfd7cc016450d8034
sonoilconte <shammersinger@yahoo.com>
2017-09-13 22:21:25 -0700

    added node and npm versions to package.json

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 let mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/translateit_test");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/translateit_test");
 
 module.exports.User = require("./user");
 module.exports.Text = require("./text");
